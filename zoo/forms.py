f1a5be89e59d3bf278cd73e0c3052f71dfc806a3
noahmagcalas <noah.magcalas@gmail.com>
2024-05-03 13:53:16 -0500

    fix datetime field widget for feeding action form

--- zoo/forms.py
+++ zoo/forms.py
@@ -1,7 +1,14 @@
 from django import forms
-from .models import FeedingAction
+from .models import FeedingAction, FeedingAppointment
 
 class AddFeedingActionForm(forms.ModelForm):
     class Meta:
         model = FeedingAction
-        fields = "__all__"
+        fields = ['exhibit', 'staff', 'date_time']
+        widgets = {
+            'date_time': forms.DateTimeInput(attrs={'type': 'datetime-local'})
+        }
+        
+# class AddFeedingAppointmentForm(forms.ModelForm):
+#     class Meta:
+#         model = FeedingAppointment
